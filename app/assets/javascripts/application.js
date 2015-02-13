b5daef8ae81b4fca7ee152316f882b12c4985668
calebsevern <calebsevern@gmail.com>
2015-02-13 18:17:17 +0000

    override textarea tabs (code + para support)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,28 @@
 //= require turbolinks
 //= require_tree .
 
+$(document).on("keydown", "textarea", function(e) {
+    if(e.keyCode === 9) { // tab was pressed
+        // get caret position/selection
+        var start = this.selectionStart;
+        var end = this.selectionEnd;
+
+        var $this = $(this);
+        var value = $this.val();
+
+        // set textarea value to: text before caret + tab + text after caret
+        $this.val(value.substring(0, start)
+                    + "\t"
+                    + value.substring(end));
+
+        // put caret at right position again (add one for the tab)
+        this.selectionStart = this.selectionEnd = start + 1;
+
+        // prevent the focus lose
+        e.preventDefault();
+    }
+});
+
 $(document).on("click",".concept", function(e) {
    window.location.replace("/concepts/" + this.id);
 });
