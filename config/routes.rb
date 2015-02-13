fc600b104c55e555e82938d4f69a791b2b814b61
Quint Guvernator <quintus.public@gmail.com>
2015-02-13 12:49:44 -0500

    fix controllers path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { sessions: 'sessions' }
+  devise_for :users, controllers: { registrations: 'users/registrations' }
 
   resources :concepts do
     resources :reviews
